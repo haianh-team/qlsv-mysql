520ae3b7c07d76c03ac56454611fd7d05868eed3
haianh123 <lehaianh111103@gmail.com>
2022-03-14 14:54:14 +0700

    1234

--- source/index.js
+++ source/index.js
@@ -49,35 +49,35 @@ app.use('/',sapxepRoute);
 app.use('/',findRoute);
 
 
-//tim khoa co nhiu hs hoc mon toan nhat 
-//tim khoa ko co ai hoc mon toan
-async function main(){
-  let findStudent = await sequelize.query(
+// //tim khoa co nhiu hs hoc mon toan nhat 
+// //tim khoa ko co ai hoc mon toan
+// async function main(){
+//   let findStudent = await sequelize.query(
     
-//     `SELECT nameKhoa,id from Khoas,(SELECT KhoaId,count(KhoaId) from Students,(SELECT DISTINCT StudentId,Subjects.nameS from StudentSubjects,Subjects,Students
-//      Where StudentSubjects.SubjectId = Subjects.id AND Subjects.nameS = 'toán') as t
-//      where id = t.StudentId
-//      group by KhoaId
-//      LIMIT 1)as k
-//      where id = k.KhoaId
+// //     `SELECT nameKhoa,id from Khoas,(SELECT KhoaId,count(KhoaId) from Students,(SELECT DISTINCT StudentId,Subjects.nameS from StudentSubjects,Subjects,Students
+// //      Where StudentSubjects.SubjectId = Subjects.id AND Subjects.nameS = 'toán') as t
+// //      where id = t.StudentId
+// //      group by KhoaId
+// //      LIMIT 1)as k
+// //      where id = k.KhoaId
+// // `
+// `  SELECT DISTINCT KhoaId from Students,
+//    (SELECT DISTINCT StudentId
+//    from StudentSubjects,Subjects
+//    where StudentSubjects.SubjectId = Subjects.id and nameS <> 'toán')as T
+//    where Students.id = T.StudentId
 // `
-`  SELECT DISTINCT KhoaId from Students,
-   (SELECT DISTINCT StudentId
-   from StudentSubjects,Subjects
-   where StudentSubjects.SubjectId = Subjects.id and nameS <> 'toán')as T
-   where Students.id = T.StudentId
-`
-,{
-    model:db.studentsubjects,
-    model:db.khoas,
+// ,{
+//     model:db.studentsubjects,
+//     model:db.khoas,
     
-    include:[{model:db.students},{model:db.subjects}],
-    type: QueryTypes.SELECT
-  })
+//     include:[{model:db.students},{model:db.subjects}],
+//     type: QueryTypes.SELECT
+//   })
  
  
-}
-main()
+// }
+// main()
 // `SELECT StudentId,Subjects.nameS from StudentSubjects,Subjects
 //  Where StudentSubjects.StudentId =  Subjects.id AND Subjects.nameS = 'toán'
 
